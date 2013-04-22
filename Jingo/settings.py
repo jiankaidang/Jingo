3871dd7806a5bfcc2fbc6e0fbd1b1fae2a64af03
jiankaidang <jiankaidang@gmail.com>
2013-04-22 17:04:17 -0400

    conflict

--- Jingo/settings.py
+++ Jingo/settings.py
@@ -120,7 +120,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    (os.path.join(os.path.dirname(__file__), 'tmp').replace('\\', '/'),),
+    (os.path.join(os.path.dirname(__file__), 'tmp').replace('\\', '/'),)
 )
 
 INSTALLED_APPS = (
