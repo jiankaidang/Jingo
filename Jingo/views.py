195e5c27a0f2e8b7ccb6de83df34d4ecad61480c
jiankaidang <jiankaidang@gmail.com>
2013-04-23 12:12:28 -0400

    login

--- Jingo/views.py
+++ Jingo/views.py
@@ -1,42 +1,43 @@
-from django.core.context_processors import csrf
 from django.shortcuts import render
 from Jingo.models import *
-from Jingo.lib.HttpRequestTasks import *
-from Jingo.lib.DataVerification import *
 
 http_res = HttpRequestResponser()
+
 
 def index(request):
     #dataverify = DataVerifier()
     #dataverify.setRulesBase()
     test = {
-            'test': 'This is Jingo Homepage.',
-            }
+        'test': 'This is Jingo Homepage.',
+    }
     return render(request, 'index.html', test)
+
 
 def pages(request, mode):
     if mode == 'signupform':
         page = 'signup.html'
-        
+    elif mode == 'login':
+        page = 'login.html'
+
     return render(request, page, {})
 
+
 def tasks(request, mode):
-    
     if mode == 'signup':
         page = 'response.html'
         data = User().signup(request)
-    
+
     if mode == 'setState':
         page = 'response.html'
         data = User().login(request)
-        
+
     if mode == 'login':
         page = 'response.html'
         data = User().login(request)
-        
+
     if mode == 'logout':
         page = 'logout.html'
         data = User().logout(request)
-        
+
     return http_res.response(page, data)
     
