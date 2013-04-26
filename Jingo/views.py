dde57bacc7d119a16609e70c8d2bf71d7fdead99
jiankaidang <jiankaidang@gmail.com>
2013-04-26 15:14:20 -0400

    comment

--- Jingo/views.py
+++ Jingo/views.py
@@ -1,16 +1,14 @@
-from django.core.context_processors import csrf
-from django.shortcuts import render
-from Jingo.lib.SQLExecution import SQLExecuter
 from Jingo.models import *
 
 http_res = HttpRequestResponser()
 
+
 def index(request):
-    data = {}
-    data['uid'] = 1
-    data['stateid'] = 0
-    data['tagid'] = 0
-    print Filter().addFilter(data)
+    # data = {}
+    # data['uid'] = 1
+    # data['stateid'] = 0
+    # data['tagid'] = 0
+    # print Filter().addFilter(data)
     #print Filter().addFilter([0, 0, None, None, 0, 0, 1])
     test = {
         'test': 'This is Jingo Homepage',
@@ -23,10 +21,10 @@ def pages(request, mode):
     data = {}
     if mode == 'signup':
         page = 'signup.html'
-        
+
     if mode == 'login':
         page = 'login.html'
-        
+
     if mode == 'profile':
         page = 'profile.html'
         #if request.session['uid']:
@@ -49,38 +47,38 @@ def tasks(request, mode):
     if mode == 'logout':
         page = 'login.html'
         data = User().logout(request)
-        
+
     if mode == 'getSysTags':
         page = 'response.html'
         data = Tag().getSysTags(request)
-    
+
     if mode == 'getUserTags':
         page = 'response.html'
         data = Tag().getUserTags(request)
-    
+
     if mode == 'addTag':
         page = 'response.html'
         data = Tag().addTag(request)
-        
+
     if mode == 'addFilter':
         page = 'response.html'
         data = Filter().addFilter(request)
-    
+
     if mode == 'getUserFilters':
         page = 'response.html'
         data = Filter().getUserFilters(request)
-        
+
     if mode == 'addInivitation':
         page = 'response.html'
         data = Friend().addInvitation(data)
-    
+
     if mode == 'getFriendInivitations':
         page = 'response.html'
         data = Friend().getFriendsInvitations(data)
-    
+
     if mode == 'getFriendsList':
         page = 'response.html'
         data = Friend().getFriendsList(data)
-        
+
     return http_res.response(request, page, data)
     
