97b8feee47356e6c0208b40bcbc847fe952e89c8
jiankaidang <jiankaidang@gmail.com>
2013-04-26 00:33:17 -0400

    profile

---
+++ Jingo/static/js/profile.js
@@ -0,0 +1,17 @@
+/**
+ * Author: Jiankai Dang
+ * Date: 4/25/13
+ * Time: 11:49 PM
+ */
+$(function () {
+    var headingClass = ".accordion-heading";
+    $(headingClass).on("click", ".icon-pencil",function () {
+        var heading = $(this).closest(headingClass);
+        heading.find("a").hide();
+        heading.find("input").show();
+    }).on("blur", "input", function () {
+            var heading = $(this).closest(headingClass);
+            heading.find("a").show();
+            heading.find("input").hide();
+        });
+});
