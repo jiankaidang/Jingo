dbe789a59da765b3f60a8d260babd5f6723c0545
jiankaidang <jiankaidang@gmail.com>
2013-05-05 15:32:55 -0400

    friends

---
+++ Jingo/static/js/friends.js
@@ -0,0 +1,30 @@
+/**
+ * Author: Jiankai Dang
+ * Date: 5/5/13
+ * Time: 2:36 PM
+ */
+$(function () {
+    var container = $("#accordion2"), uid = container.attr("data-uid");
+    container.on("click", ".unfollow-friend",function () {
+        $.post("/tasks/unfollow/", {
+            uid: uid,
+            f_uid: $(this).attr("f_uid")
+        }, function () {
+            location.href.reload();
+        });
+    }).on("click", ".accept-friend",function () {
+            $.post("/tasks/replyInvitation/", {
+                invitationid: $(this).closest("li").attr("invitationid"),
+                reply: 1
+            }, function () {
+                location.href.reload();
+            });
+        }).on("click", ".deny-friend", function () {
+            $.post("/tasks/replyInvitation/", {
+                invitationid: $(this).closest("li").attr("invitationid"),
+                reply: 0
+            }, function () {
+                location.href.reload();
+            });
+        });
+});
